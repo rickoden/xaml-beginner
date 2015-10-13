3b8597e972d3a3f03b89797fc944a3dc66e38a63
Rickie Oden <rick.oden@msn.com>
2015-10-12 20:46:37 -0600

    Binding
    Bind to the DataManager

---
+++ RestaurantManager/RestaurantManager/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RestaurantManager
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Frame rootFrame = Window.Current.Content as Frame;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            if (b != null)
+            {
+                string tag = b.Tag.ToString();
+                if ("GoToOrders" == tag)
+                {
+                    this.Frame.Navigate(typeof(ExpeditePage));
+                }
+                else
+                {
+                    if ("GoToSubmit" == tag)
+                    {
+                        this.Frame.Navigate(typeof(OrderPage));
+                    }
+                }
+            }
+        }
+
+    }
+}
